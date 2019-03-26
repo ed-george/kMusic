be07b43c3ff042cbae69a455bc49817416d81ad3
Ed George <ed.george@himumsaiddad.com>
2019-03-26 19:24:17 +0000

    Add additional note tests

--- kmusic-core/src/test/kotlin/uk/co/edgeorgedev/kmusic/data/NoteTest.kt
+++ kmusic-core/src/test/kotlin/uk/co/edgeorgedev/kmusic/data/NoteTest.kt
@@ -1,6 +1,6 @@
 package uk.co.edgeorgedev.kmusic.data
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /*
@@ -35,6 +35,33 @@ class NoteTest {
         val note1 = Note(Pitch.E5, Duration.SIXTEENTH_NOTE)
         val note2 = Note(Pitch.E5, Duration.SIXTEENTH_NOTE)
         assertEquals(note1, note2)
+    }
+
+    @Test
+    fun `rest creation`() {
+        val rest = Rest(Duration.DEMI_SEMI_QUAVER)
+        assertEquals(Duration.DEMI_SEMI_QUAVER, rest.duration)
+    }
+
+    @Test
+    fun `rest equality`() {
+        val rest1 = Rest(Duration.SIXTEENTH_NOTE)
+        val rest2 = Rest(Duration.SIXTEENTH_NOTE)
+        assertEquals(rest1, rest2)
+    }
+
+    @Test
+    fun `chord creation`() {
+        val chord = Chord(duration = Duration.HALF_NOTE, notes = listOf(Note(pitch = Pitch.A3), Note(pitch = Pitch.C3)))
+        assertEquals(listOf(Note(pitch = Pitch.A3), Note(pitch = Pitch.C3)), chord.notes)
+        assertEquals(Duration.HALF_NOTE, chord.duration)
+    }
+
+    @Test
+    fun `chord equality`() {
+        val chord1 = Chord(duration = Duration.HALF_NOTE, notes = listOf(Note(pitch = Pitch.A3), Note(pitch = Pitch.C3)))
+        val chord2 = Chord(duration = Duration.HALF_NOTE, notes = listOf(Note(pitch = Pitch.A3), Note(pitch = Pitch.C3)))
+        assertEquals(chord1, chord2)
     }
 
 }
